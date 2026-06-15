2dd9d3270b9c3541ac5b8c52afa879b3f2919d36
saleh <profsaleh88@gmail.com>
2026-06-15 07:22:27 +0300

    Work In MVC Project

---
+++ ClinicWebMVC/Controllers/BaseController.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClinicWebMVC.Controllers
+{
+    // نجعله Abstract لأنه كلاس قاعدي لا يُستخدم مباشرة
+    public abstract class BaseController : Controller
+    {
+        private IMediator? _mediator;
+
+        // نستخدم HttpContext.RequestServices للحصول على الـ Mediator
+        // هذا يقلل من تكرار كتابة الـ Constructor في كل كنترولر
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetRequiredService<IMediator>();
+    }
+}
---
+++ ClinicWebMVC/Controllers/BillsController.cs
@@ -0,0 +1,43 @@
+﻿using ClinicServices.Bills.Commands.CreateBill;
+using ClinicServices.Bills.Commands.PayBill;
+using ClinicServices.Bills.Queries.GetAllBills;
+using ClinicServices.Bills.Queries.GetBillById;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClinicWebMVC.Controllers
+{
+    public class BillsController : BaseController
+    {
+        // عرض قائمة الفواتير
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var bills = await Mediator.Send(new GetAllBillsQuery());
+            return View(bills);
+        }
+
+        // عرض تفاصيل فاتورة
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Details(int id)
+        {
+            var bill = await Mediator.Send(new GetBillByIdQuery(id));
+            return View("Details", bill);
+        }
+
+        // إنشاء فاتورة جديدة
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateBillCommand command)
+        {
+            await Mediator.Send(command);
+            return RedirectToAction(nameof(Index));
+        }
+
+        // دفع فاتورة
+        [HttpPost]
+        public async Task<IActionResult> Pay(int id)
+        {
+            await Mediator.Send(new PayBillCommand(id));
+            return RedirectToAction(nameof(Details), new { id });
+        }
+    }
+}
